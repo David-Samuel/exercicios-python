ceb30dd244dc016b559c0b83e16279e7ef5f41a7
David-Samuel <69125656+David-Samuel@users.noreply.github.com>
2020-12-12 19:49:05 -0300

    Validação de CPF fase 2
    Hoje mesmo dei uma pequena atualizada no exercício, eliminei algumas linhas desnecessárias e juntei variáveis que tinham o mesmo valor, também adicionei comentários para não me confundir no processo de atualizações. Por vez é isso, ainda não tenho ideias de como melhorá-lo, mas acredito que com o tempo eu possa voltar aqui e testar minha evolução.

--- validação_de_cpf.py
+++ validação_de_cpf.py
@@ -1,14 +1,11 @@
 cpf = str(input('\033[33mInforme seu CPF: \033[m'))
-cont = 0
-add = 0
-nx = 10
-end = 10
+cont = add = 0 #cont = variável utilizada para indicar um índice dentro de "cpf" / add = variável que faz a soma das múltiplas "mult" para ser utilizada na fórmula
+nx = end = 10 #nx = número a ser multiplicado pelos algorismos em "cpf" que diminui a cada volta no laço / end = variável que indica o final para zerar os valores e realizar a fórmula
 validation = list()
 final_cpf = list()
 
 if len(cpf) == 11:
-    final_cpf.append(int(cpf[9]))
-    final_cpf.append(int(cpf[10]))
+    final_cpf = [cpf[9], cpf[10]]
     while True:
         if nx == 12:
             break
@@ -16,19 +13,18 @@ if len(cpf) == 11:
         mult = num * nx 
         add += mult
         cont += 1
-        nx -= 1
+        nx -= 1 
         if cont == end - 1 and end < 12:
-            valid = 11 - (add % 11)
+            valid = 11 - (add % 11) #Fórmula para validação dos últimos dígitos do CPF
             end += 1
             nx = end
-            cont = add = 0
-            mult = 1
+            cont = add = mult = 0
             if valid > 9:
                 valid = 0
-            validation.append(valid)
+            validation.append(str(valid))
 
 if final_cpf == validation and len(cpf) == 11:
     print('\033[32mVálido!\033[m')
 else:
     print('\033[31mInválido!\033[m')
-print('\033[36mPrograma finalizado!\033[m')  
+print('\033[36mPrograma finalizado!\033[m') 
