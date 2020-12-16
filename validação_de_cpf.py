1e66c12be6a7e77f1ad2ef796517b4d9b5d2a399
David-Samuel <69125656+David-Samuel@users.noreply.github.com>
2020-12-16 17:32:33 -0300

    Validação de CPF fase 3
    Apenas uma leve mudança em que agora o laço é infinito, para que não seja necessário rodar o programa novamente a toda hora.

--- validação_de_cpf.py
+++ validação_de_cpf.py
@@ -1,30 +1,28 @@
-cpf = str(input('\033[33mInforme seu CPF: \033[m'))
-cont = add = 0 #cont = variável utilizada para indicar um índice dentro de "cpf" / add = variável que faz a soma das múltiplas "mult" para ser utilizada na fórmula
-nx = end = 10 #nx = número a ser multiplicado pelos algorismos em "cpf" que diminui a cada volta no laço / end = variável que indica o final para zerar os valores e realizar a fórmula
-validation = list()
-final_cpf = list()
+while True:
+    cpf = str(input('\033[33mInforme seu CPF: \033[m'))
+    cont = add = 0 #cont = variável utilizada para indicar um índice dentro de "cpf" / add = variável que faz a soma das múltiplas "mult" para ser utilizada na fórmula
+    nx = end = 10 #nx = número a ser multiplicado pelos algorismos em "cpf" que diminui a cada volta no laço / end = variável que indica o final para zerar os valores e realizar a fórmula
+    other_cpf = cpf[:9]
+    if len(cpf) == 11:
+        while True:
+            if nx == 12:
+                break
+            mult = int(cpf[cont]) * nx 
+            add += mult
+            cont += 1
+            nx -= 1 
+            if cont == end - 1 and end < 12:
+                valid = 11 - (add % 11) #Fórmula para validação dos últimos dígitos do CPF
+                end += 1
+                nx = end
+                cont = add = mult = 0
+                if valid > 9:
+                    valid = 0
+                other_cpf += str(valid)
+    sequence = other_cpf == str(other_cpf[0]) * len(cpf)
 
-if len(cpf) == 11:
-    final_cpf = [cpf[9], cpf[10]]
-    while True:
-        if nx == 12:
-            break
-        num = int(cpf[cont]) 
-        mult = num * nx 
-        add += mult
-        cont += 1
-        nx -= 1 
-        if cont == end - 1 and end < 12:
-            valid = 11 - (add % 11) #Fórmula para validação dos últimos dígitos do CPF
-            end += 1
-            nx = end
-            cont = add = mult = 0
-            if valid > 9:
-                valid = 0
-            validation.append(str(valid))
-
-if final_cpf == validation and len(cpf) == 11:
-    print('\033[32mVálido!\033[m')
-else:
-    print('\033[31mInválido!\033[m')
-print('\033[36mPrograma finalizado!\033[m') 
+    if other_cpf == cpf and not sequence:
+        print('\033[32mVálido!\033[m')
+    else:
+        print('\033[31mInválido!\033[m')
+    print('\033[36mPrograma finalizado!\033[m') 
